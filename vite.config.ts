696f0de5c998bb877d170985b90b844e2584e198
KarinaI <7karina.ibr@gmail.com>
2024-05-02 16:41:16 +0300

    fix: fix config files

--- vite.config.ts
+++ vite.config.ts
@@ -8,26 +8,15 @@ import stylelint from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools(), eslint(), stylelint()],
+  plugins: [
+    vue(),
+    process.env.NODE_ENV === 'development' ? VueDevTools() : undefined,
+    eslint(),
+    stylelint()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
-
-// import { defineConfig } from 'vite';
-// import tsconfigPaths from 'vite-tsconfig-paths';
-
-// export default defineConfig({
-//   base: './',
-//   plugins: [tsconfigPaths()],
-//   css: {
-//     modules: {
-//       localsConvention: 'camelCase',
-//     },
-//   },
-//   build: {
-//     target: 'ES2022',
-//   },
-// });
