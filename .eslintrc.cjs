7d4c2349003f7b63bc27aa027c3f8335989e1693
KarinaI <7karina.ibr@gmail.com>
2024-05-03 01:26:38 +0300

    fix: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,6 @@ module.exports = {
   },
   rules: {
     'vue/multi-word-component-names': 'off',
-    'prettier/prettier': ['error'],
     'no-console': ['error', { allow: ['warn', 'error'] }],
   },
 }
