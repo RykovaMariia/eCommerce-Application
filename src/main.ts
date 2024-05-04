361612a0e54b842ab8c1623f35c054f2af1540b0
Mariia Rykova <maryrykova24@gmail.com>
2024-05-04 22:20:20 +0400

    feat: configure styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './styles/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,17 @@ export default defineConfig({
     vue(),
     process.env.NODE_ENV === 'development' ? VueDevTools() : undefined,
     eslint(),
-    stylelint()
+    stylelint(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+    },
+  },
 })
