b52c647f6bf3372ba6d89081052d4f599c8444b6
ChickenMcNuggetss <95967653+ChickenMcNuggetss@users.noreply.github.com>
2024-05-05 18:02:47 +0300

    feat: add vuetify (#14)

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
     <div class="wrapper">
       <HelloWorld msg="Hello" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import './styles/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
