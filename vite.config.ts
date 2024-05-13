9abee89b536ebe5e6d69d7b2c1213dfd4166dbbb
YuliyaVoronovich <Y-1306@mail.ru>
2024-05-13 10:55:22 +0300

    refactor: remove comments

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-//import eslint from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 
   plugins: [
     vue(),
     process.env.NODE_ENV === 'development' ? VueDevTools() : undefined,
-    //eslint(),
+    eslint(),
     stylelint(),
   ],
   define: {
