8919b9675c10c85a1f5f59734038c6c1f8b240d4
Julia <Y-1306@mail.ru>
2024-05-15 15:24:07 +0300

    Feat: add api SDK services (#18)
    * feat: add sdk Client
    
    * refactor: change BuilderClient to class Client
    
    * feat: add store for auth
    
    * feat: add refresh token
    
    * fix: remove unnecessary dependencies
    
    * refactor: remove comments
    
    * fix: correct Client and state user
    
    * fix: add remove component
    
    * refactor: made Client as singlton
    
    * refactor: change class Client
    
    * refactor: default parametr for apiRoot
    
    * fix: refactor Client class
    
    * fix: correct class Client change taking Loggining store
    
    * refactor: change name class Client to ClientService

---
+++ src/services/storage-service.ts
@@ -0,0 +1,52 @@
+import type { TokenStore } from '@commercetools/sdk-client-v2'
+
+class StorageService<T> {
+  constructor(private storageKeyPrefix: string) {}
+
+  private getStorageKey(key: string): string {
+    return `${this.storageKeyPrefix}_${key}`
+  }
+
+  saveData<K extends keyof T>(key: K, data: T[K]): void {
+    const storageKey = this.getStorageKey(key.toString())
+    localStorage.setItem(storageKey, JSON.stringify(data))
+  }
+
+  removeData<K extends keyof T>(key: K): void {
+    const storageKey = this.getStorageKey(key.toString())
+    localStorage.removeItem(storageKey)
+  }
+
+  getData<K extends keyof T>(key: K, validate?: (data: unknown) => data is T[K]): T[K] | null {
+    const storageKey = this.getStorageKey(key.toString())
+    const data = localStorage.getItem(storageKey)
+
+    if (data === null) {
+      return null
+    }
+    try {
+      const result: unknown = JSON.parse(data)
+      if (validate) {
+        return validate(result) ? result : null
+      }
+      return result as T[K]
+    } catch (error) {
+      console.error('Error parsing JSON:', error)
+      return null
+    }
+  }
+
+  toggleData<K extends keyof T>(key: K, data: T[K]): void {
+    if (this.getData(key)) {
+      this.removeData(key)
+    } else {
+      this.saveData(key, data)
+    }
+  }
+}
+
+export type LocalStorage = {
+  token: TokenStore
+}
+
+export const localStorageService = new StorageService<LocalStorage>('ECommerce_')
