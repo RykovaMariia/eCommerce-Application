4ad0243550444c923530610f0f2cc60ce33c4573
KarinaI <7karina.ibr@gmail.com>
2024-05-18 09:46:07 +0300

    refactor: refactor alert component

--- src/components/alertWindow/AlertWindow.vue
+++ src/components/alertWindow/AlertWindow.vue
@@ -6,15 +6,13 @@ const props = defineProps<AlertProps>()
 </script>
 
 <template>
-  <v-banner
-      lines="three"
-    >
-      <v-banner-text>
-        {{ props.textContent }}
-      </v-banner-text>
+  <v-banner lines="three">
+    <v-banner-text>
+      {{ props.textContent }}
+    </v-banner-text>
 
-      <template v-slot:actions>
-        <v-btn>Action</v-btn>
-      </template>
-    </v-banner>
+    <template v-slot:actions>
+      <v-btn>Action</v-btn>
+    </template>
+  </v-banner>
 </template>
