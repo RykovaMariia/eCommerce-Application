2bb9cbed9277911bfdd5a89ab0a72ee2c189c665
KarinaI <7karina.ibr@gmail.com>
2024-05-18 09:45:14 +0300

    feat: add alert component

---
+++ src/components/alertWindow/AlertWindow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface AlertProps {
+  textContent?: string
+}
+const props = defineProps<AlertProps>()
+</script>
+
+<template>
+  <v-banner
+      lines="three"
+    >
+      <v-banner-text>
+        {{ props.textContent }}
+      </v-banner-text>
+
+      <template v-slot:actions>
+        <v-btn>Action</v-btn>
+      </template>
+    </v-banner>
+</template>
