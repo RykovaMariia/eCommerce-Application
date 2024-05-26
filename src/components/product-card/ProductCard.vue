955673e66296c3639eeb715af25484fda74b8b71
Mariia Rykova <maryrykova24@gmail.com>
2024-05-26 16:16:38 +0400

    feat: add product card

---
+++ src/components/product-card/ProductCard.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import Button from '../buttons/Button.vue'
+defineProps<{
+  src?: string
+  name?: string
+  description?: string
+  price?: number
+  discountedPrice?: number
+}>()
+</script>
+<template>
+  <v-card elevation="0" max-width="290" variant="text" class="product-card">
+    <v-img height="340" :src="src" cover></v-img>
+    <v-card-title>{{ name }}</v-card-title>
+    <v-card-subtitle opacity="1">{{ description }} </v-card-subtitle>
+    <v-card-text v-if="!discountedPrice" class="price">€{{ price }}</v-card-text>
+    <v-card-text v-if="discountedPrice" class="price discount"
+      >€{{ discountedPrice }} <span class="line-through">€{{ price }}</span></v-card-text
+    >
+    <v-card-actions>
+      <Button textContent="Add to card" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+@use '@/styles/constants.scss';
+
+.product-card {
+  cursor: pointer;
+}
+
+.v-img {
+  border: 1px solid constants.$color-primary;
+  border-radius: 10px;
+}
+
+.v-card-title {
+  padding: 0.5rem 0;
+  white-space: normal;
+  font-size: 1.15rem;
+}
+
+.v-card-subtitle {
+  padding: 0;
+}
+
+.v-card-text {
+  padding: 0.5rem 0;
+  font-size: 1.15rem;
+}
+
+.price {
+  font-weight: 500;
+}
+
+.discount {
+  color: constants.$color-sale;
+}
+
+.line-through {
+  color: constants.$color-text-dark;
+  opacity: 0.8;
+  text-decoration: line-through;
+}
+
+.v-card-actions {
+  padding: 0;
+}
+
+.v-btn {
+  padding: 0.5rem 2rem;
+  font-size: 0.8rem;
+  border-radius: 10px;
+}
+</style>
--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -1,3 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ProductProjection } from '@commercetools/platform-sdk'
+import { productsService } from '../services/productsService'
+import Breadcrumb from '@components/breadcrumbs/Breadcrumb.vue'
+import ProductCard from '@components/product-card/ProductCard.vue'
+import { onMounted, ref, type Ref } from 'vue'
 
-<template><h1>Catalog</h1></template>
+const items = [
+  {
+    title: 'Main',
+    disabled: false,
+    href: '/',
+  },
+  {
+    title: 'Catalog',
+    disabled: true,
+    href: '/catalog',
+  },
+]
+
+const limit = 20
+
+let products: Ref<ProductProjection[]> = ref([])
+let productsCount: Ref<number> = ref(0)
+
+onMounted(() => {
+  productsService
+    .products(limit, 30)
+    .then((response) => {
+      products.value = response.body.results
+      productsCount.value = response.body.total || 0
+    })
+    .catch((error: Error) => {
+      throw new Error(error.message)
+    })
+})
+</script>
+
+<template>
+  <Breadcrumb :items="items" />
+  <div class="d-flex">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :src="product.masterVariant.images?.[0]?.url"
+      :name="product.name['en-GB']"
+      :description="product.description?.['en-GB']"
+      :price="(product.masterVariant.prices?.[0]?.value?.centAmount ?? 0) / 100"
+      :discountedPrice="
+        (product.masterVariant.prices?.[0]?.discounted?.value.centAmount ?? 0) / 100
+      "
+    />
+  </div>
+  <v-pagination
+    :length="Math.ceil(productsCount / limit)"
+    color="primary"
+    density="comfortable"
+  ></v-pagination>
+</template>
+
+<style lang="scss" scoped>
+.d-flex {
+  flex-wrap: wrap;
+  gap: 2.5rem;
+  justify-content: center;
+}
+</style>
