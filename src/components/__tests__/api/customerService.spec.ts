3f07840a168173c6ab03c30fefc53b12f5921143
YuliyaVoronovich <Y-1306@mail.ru>
2024-05-27 12:27:41 +0300

    feat: add test fo customerService update method

--- src/components/__tests__/api/customerService.spec.ts
+++ src/components/__tests__/api/customerService.spec.ts
@@ -1,6 +1,11 @@
-import { describe, it, expect, afterEach, vi } from 'vitest'
+import { describe, it, expect, afterEach, vi, beforeEach } from 'vitest'
 import { CustomerService } from '@/services/customerService'
 import { ClientService } from '@/api/ClientService'
+import { createPinia, setActivePinia } from 'pinia'
+
+beforeEach(() => {
+  setActivePinia(createPinia())
+})
 
 describe('Cusomer Service', () => {
   const ClientServiceMock = {
@@ -27,6 +32,79 @@ describe('Cusomer Service', () => {
   it('should return a user', async () => {
     const result = await customerService.user()
     expect(result).toEqual({})
+    expect(ClientServiceMock.getApiRoot).toHaveBeenCalledOnce
+    expect(ClientServiceMock.getClient).toHaveBeenCalledOnce
+  })
+})
+
+describe('Cusomer Service', () => {
+  const ClientServiceMock = {
+    getApiRoot: vi.fn(() => {
+      return {
+        me: () => {
+          return {
+            post: () => {
+              return { execute: () => Promise.resolve({}) }
+            },
+          }
+        },
+      }
+    }),
+    getClient: vi.fn(),
+  }
+
+  const customerService = new CustomerService(ClientServiceMock as unknown as ClientService)
+
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should call the clientService methods with the correct data', async () => {
+    const userData = {
+      id: '',
+      createdAt: '',
+      lastModifiedAt: '',
+      lastModifiedBy: {
+        clientId: '',
+        anonymousId: '',
+      },
+      createdBy: {
+        clientId: '',
+        anonymousId: '',
+      },
+      email: '',
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      password: '',
+      addresses: [],
+      shippingAddressIds: [],
+      billingAddressIds: [],
+      isEmailVerified: false,
+      stores: [],
+      authenticationMode: '',
+      version: 1,
+      actions: [
+        {
+          action: 'setFirstName',
+          firstName: 'Vasya',
+        },
+        {
+          action: 'setLastName',
+          lastName: 'Ivanov',
+        },
+        {
+          action: 'setDateOfBirth',
+          dateOfBirth: '1995-02-16',
+        },
+        {
+          action: 'changeEmail',
+          email: 'test@example.com',
+        },
+      ],
+    }
+
+    await customerService.update(userData)
     expect(ClientServiceMock.getApiRoot).toHaveBeenCalledOnce
     expect(ClientServiceMock.getClient).toHaveBeenCalledOnce
   })
