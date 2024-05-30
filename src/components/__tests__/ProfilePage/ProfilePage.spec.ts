99e8268753fbd9e70536f0c7297c3d5a74bef092
YuliyaVoronovich <Y-1306@mail.ru>
2024-05-30 11:56:51 +0300

    refactor: refactor test Profile Page

--- src/components/__tests__/ProfilePage/ProfilePage.spec.ts
+++ src/components/__tests__/ProfilePage/ProfilePage.spec.ts
@@ -1,9 +1,7 @@
 import { describe, it, expect, beforeAll } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import ProfileEditForm from '@/pages/ProfilePage/components/ProfileEditForm.vue'
 import ProfileAddress from '@/pages/ProfilePage/components/ProfileAddress.vue'
-import PasswordEditForm from '@/pages/ProfilePage/components/PasswordEditForm.vue'
 import ProfilePage from '@pages/ProfilePage/ProfilePage.vue'
 import { ResizeObserverMock, vuetify } from '@/utils/testUtils'
 import { createPinia } from 'pinia'
@@ -29,20 +27,5 @@ describe('ProfilePage', () => {
       },
     })
     expect(wrapper.findComponent(ProfileAddress).exists()).toBeTruthy
-  })
-
-  it('check props', () => {
-    const emailTest = 'test@example.com'
-
-    const wrapper = mount(PasswordEditForm, {
-      global: {
-        plugins: [createPinia(), vuetify],
-      },
-      propsData: {
-        email: emailTest,
-      },
-    })
-    //@ts-ignore
-    expect(wrapper.props().email).toBe(emailTest)
   })
 })
