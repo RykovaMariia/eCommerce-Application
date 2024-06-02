e8a3d1e09f64dd9b8996ff3f01302f37ad011094
YuliyaVoronovich <Y-1306@mail.ru>
2024-06-02 19:21:36 +0300

    refactor: add alignment icon with text

--- src/pages/ProfilePage/components/AddressList.vue
+++ src/pages/ProfilePage/components/AddressList.vue
@@ -66,11 +66,11 @@ function setAsDefault(address: Address) {
 <template>
   <v-list bg-color="transparent" color="primary">
     <v-list-item class="item" v-for="(item, i) in items" :key="i" :value="item" color="primary">
+      <template v-slot:prepend>
+        <v-icon color="primary" icon="mdi-home-map-marker" size="small" class="icon"></v-icon>
+      </template>
       <v-list-item-title @click="setAsDefault(item)">
-        <div class="text">
-          <v-icon color="primary" icon="mdi-home-map-marker" size="small" class="icon"></v-icon>
-          {{ item.city }}, {{ item.streetName }}, {{ item.postalCode }}
-        </div>
+        <div class="text">{{ item.city }}, {{ item.streetName }}, {{ item.postalCode }}</div>
       </v-list-item-title>
 
       <template v-slot:append>
@@ -97,6 +97,10 @@ function setAsDefault(address: Address) {
 <style scoped lang="scss">
 @use '@/styles/constants.scss';
 @use '@/styles/mixins.scss';
+
+::v-deep(.v-list-item__prepend > .v-icon ~ .v-list-item__spacer) {
+  width: 0.5rem;
+}
 
 @include mixins.media-middle {
   .v-list-item--density-default:not(.v-list-item--nav).v-list-item--one-line {
