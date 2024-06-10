08ca46864ee1e320d2f94a86adf05fc74c6ffdfa
Mariia Rykova <maryrykova24@gmail.com>
2024-06-10 19:31:10 +0400

    feat: fetch product

--- src/pages/CartPage/CartPage.vue
+++ src/pages/CartPage/CartPage.vue
@@ -3,76 +3,49 @@ import ProductInCart from '@/components/productIn-cart/ProductInCart.vue'
 import Input from '@/components/inputs/Input.vue'
 import Button from '@/components/buttons/Button.vue'
 import { ref } from 'vue'
+import { useCartStore } from '@/stores/cart'
+import { storeToRefs } from 'pinia'
 import { FULL_PERCENTAGE } from '@/constants/constants'
 
-const products = [
-  {
-    srcImg:
-      'https://zerowastestore.com/cdn/shop/files/sustain-yourself-sensitive-skin-deodorant-3-scent-options-32612745314415.jpg?v=1697130835&width=1200',
-    name: 'Sensitive Skin Deodorant',
-    description:
-      'Perfect for those with sensitive skin, this effective, all-natural deodorant is made with gentle ingredients & is baking soda free. Gentle & long-lasting, this deodorant is the perfect zero waste alternative to chemically-filled deodorant that comes in plastic. Magnesium hydroxide is added for its odor fighting benefits.',
-    price: 799,
-    discountedPrice: 766,
-    slug: 'muslin-produce-ba',
-    key: '201',
-  },
-  {
-    srcImg:
-      'https://zerowastestore.com/cdn/shop/files/sustain-yourself-sensitive-skin-deodorant-3-scent-options-32612745183343.jpg?v=1697130655&width=1200',
-    name: 'Sensitive Skin Deodorant',
-    description:
-      'Perfect for those with sensitive skin, this effective, all-natural deodorant is made with gentle ingredients & is baking soda free. Gentle & long-lasting, this deodorant is the perfect zero waste alternative to chemically-filled deodorant that comes in plastic. Magnesium hydroxide is added for its odor fighting benefits.',
-    price: 799,
-    slug: 'mesh-produce-bag',
-    key: '200',
-  },
-]
-
-const totalPrice = () => {
-  return (
-    products.reduce(
-      (total, product) =>
-        total + (product.discountedPrice ? +product.discountedPrice : product.price),
-      0,
-    ) / FULL_PERCENTAGE
-  )
-}
+const { cart } = storeToRefs(useCartStore())
 
 const promocode = ref('')
 </script>
 
 <template>
-  <ProductInCart
-    v-for="product in products"
-    :key="product.name"
-    :srcImg="product.srcImg"
-    :name="product.name"
-    :price="product.price"
-    :discountedPrice="product.discountedPrice ?? undefined"
-    :productSlug="product.slug"
-    :productKey="product.key"
-  />
-  <div class="d-flex cart-total">
-    <v-form>
-      <div class="d-flex promocode">
-        <Input
-          class="promocode-input"
-          label="Enter your promo code:"
-          placeholder="Your promo code"
-          v-model="promocode"
-          isHideDetails="auto"
-        />
-        <Button
-          textContent="Apply"
-          variant="outlined"
-          classes="primary"
-          buttonType="submit"
-          :disabled="!promocode"
-        />
-      </div>
-    </v-form>
-    <div class="total-price">Total: €{{ totalPrice() }}</div>
+  <div v-if="cart?.totalLineItemQuantity">
+    <ProductInCart
+      v-for="product in cart.lineItems"
+      :key="product.name['en-GB']"
+      :srcImg="product.variant.images?.[0].url ?? ''"
+      :name="product.name['en-GB']"
+      :price="product.price.value.centAmount"
+      :discountedPrice="product.price.discounted?.value.centAmount ?? 0"
+      :productSlug="product.productSlug?.['en-GB'] ?? ''"
+      :productKey="product.productKey ?? ''"
+    />
+
+    <div class="d-flex cart-total">
+      <v-form>
+        <div class="d-flex promocode">
+          <Input
+            class="promocode-input"
+            label="Enter your promo code:"
+            placeholder="Your promo code"
+            v-model="promocode"
+            isHideDetails="auto"
+          />
+          <Button
+            textContent="Apply"
+            variant="outlined"
+            classes="primary"
+            buttonType="submit"
+            :disabled="!promocode"
+          />
+        </div>
+      </v-form>
+      <div class="total-price">Total: €{{ cart.totalPrice.centAmount / FULL_PERCENTAGE }}</div>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
