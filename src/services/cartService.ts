f8af1e00cda7c22faa3c00dde464d390705c171c
YuliyaVoronovich <Y-1306@mail.ru>
2024-06-11 22:35:37 +0300

    refactor: add erly return

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -10,21 +10,22 @@ export class CartService {
   constructor(private cartApiService: CartApiService) {}
 
   public setAnonymousSession() {
-    if (cartId) {
-      cartApiService
-        .getCartById(cartId)
-        .then(({ body }) => {
-          if (anonymousId && body.createdBy?.anonymousId !== anonymousId) {
-            return cartApiService
-              .updateAnonymousId({ id: cartId, version: body.version, anonymousId })
-              .then(({ body }) => body)
-          }
-          return body
-        })
-        .then((body) => {
-          useCartStore().setCart(body)
-        })
+    if (!cartId) {
+      return
     }
+    cartApiService
+      .getCartById(cartId)
+      .then(({ body }) => {
+        if (anonymousId && body.createdBy?.anonymousId !== anonymousId) {
+          return cartApiService
+            .updateAnonymousId({ id: cartId, version: body.version, anonymousId })
+            .then(({ body }) => body)
+        }
+        return body
+      })
+      .then((body) => {
+        useCartStore().setCart(body)
+      })
   }
 
   public createCart() {
