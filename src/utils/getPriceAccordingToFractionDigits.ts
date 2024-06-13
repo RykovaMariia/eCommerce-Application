3ae761885cce76c2eb6bac3ac9cb20881a470fa3
Mariia Rykova <maryrykova24@gmail.com>
2024-06-13 23:34:07 +0400

    refactor: refactor badge

--- src/utils/getPriceAccordingToFractionDigits.ts
+++
@@ -1,8 +0,0 @@
-import type { TypedMoney } from '@commercetools/platform-sdk'
-
-export function getPriceAccordingToFractionDigits(value?: TypedMoney) {
-  if (value) {
-    return value.centAmount / 10 ** value.fractionDigits
-  }
-  return 0
-}
