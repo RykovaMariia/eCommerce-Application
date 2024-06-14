5384281f7a86a745e2374103ef02a285729fdae6
KarinaI <7karina.ibr@gmail.com>
2024-06-14 03:31:08 +0300

    feat: add categories on main page

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -2,10 +2,18 @@
 import CarouselImage1 from '@assets/images/main/main-carousel1.jpg'
 import CarouselImage2 from '@assets/images/main/main-carousel2.jpg'
 import CarouselImage3 from '@assets/images/main/main-carousel3.jpg'
+import Kitchen from '@assets/images/main/kitchen.jpg'
+import BathBody from '@assets/images/main/bathBody.jpg'
+import Bedding from '@assets/images/main/bedding.jpg'
+import IconLeaf from '@components/icons/IconLeaf.vue'
+import IconGift from '@components/icons/IconGift.vue'
 import IconRocket from '@components/icons/IconRocket.vue'
-import Button from '@components/buttons/Button.vue'
+import IconLicense from '@components/icons/IconLicense.vue'
 import { useCategoriesStore } from '@/stores/categories'
 import { storeToRefs } from 'pinia'
+import AdvantageCard from '@pages/MainPage/components/AdvantageCard.vue'
+import CategoryCard from '@pages/MainPage/components/CategoryCard.vue'
+import { RouterLink } from 'vue-router'
 
 const { categoriesLinks } = storeToRefs(useCategoriesStore())
 
@@ -13,70 +21,75 @@ const INTERVAL_DURATION = 6000
 
 const carouselImages = [CarouselImage1, CarouselImage2, CarouselImage3]
 
-const titleGreeting = ['Welcome', 'Sustainability and style', 'Discover organic products']
+const categoryImages = [Kitchen, BathBody, Bedding]
+
+const titleGreeting = ['Welcome', 'Sustainability and style', 'Discover organic products'] as const
+
+const advantages = [
+  { icon: IconLeaf, text: 'Eco friendly goods' },
+  {
+    icon: IconGift,
+    text: 'Eco packaging',
+  },
+  {
+    icon: IconRocket,
+    text: 'Fast delivery',
+  },
+  {
+    icon: IconLicense,
+    text: 'Certificated products',
+  },
+] as const
 </script>
 
 <template>
   <div class="title">
     <div class="title_upper-line">Green city style</div>
-    <div class="title_bottom-line">Eco goods store </div>
+    <div class="title_bottom-line">Eco goods store</div>
   </div>
-  <div class="carousel-wrapper">
-    <v-carousel
-      cycle
-      :interval="INTERVAL_DURATION"
-      class="carousel"
-      :show-arrows="false"
-      :touch="true"
-    >
+  <div class="wrapper">
+    <v-carousel :interval="INTERVAL_DURATION" class="carousel" :show-arrows="false" :touch="true">
       <v-carousel-item v-for="(url, n) in carouselImages" :key="url" :src="url" cover>
-        <v-col class="wrapper">
-          <div class="container">
-            <h1 class="title-greeting" v-if="n === 0">{{ titleGreeting[n] }}</h1>
-            <div class="carousel-text" v-else>{{ titleGreeting[n] }}</div>
-            <Button
-              textContent="Go to catalog"
-              to="/catalog"
-              color="secondary"
-              size="large"
-              class="button"
-              append-icon="mdi-chevron-right"
-            />
-          </div>
+        <v-col class="carousel-item-wrapper">
+          <h1 class="title-greeting" v-if="n === 0">{{ titleGreeting[n] }}</h1>
+          <div class="carousel-text" v-else>{{ titleGreeting[n] }}</div>
+          <RouterLink to="/catalog" class="text"
+            >Go to catalog <v-icon icon="mdi-chevron-right"></v-icon
+          ></RouterLink>
         </v-col>
       </v-carousel-item>
     </v-carousel>
   </div>
 
-  <IconRocket />
-
-  <v-col>
-    <h2>Categories</h2>
-    <div v-for="(link, index) in categoriesLinks" :key="index" class="category">
-      <RouterLink :to="link.parent.href">{{ link.parent.name }}</RouterLink>
+  <div class="advantages-wrapper">
+    <div v-for="{ icon, text } in advantages" :key="text">
+      <AdvantageCard :Icon="icon" :text="text" />
     </div>
-  </v-col>
-  <!-- <div class="links">
-    <RouterLink to="/login">Login</RouterLink>
-    <RouterLink to="/registration">Registration</RouterLink>
-    <RouterLink to="/catalog">Catalog</RouterLink>
-    <RouterLink to="/profile">Profile</RouterLink>
-    <RouterLink to="/cart">Cart</RouterLink>
-    <RouterLink to="/about">About us</RouterLink>
-    <RouterLink to="/favorites">Favorites</RouterLink>
-    <RouterLink to="/product">Product</RouterLink>
-  </div> -->
+  </div>
+
+  <div class="wrapper">
+    <h2 class="title-category">Categories</h2>
+    <div class="categories">
+      <div v-for="(link, index) in categoriesLinks.slice(1)" :key="index">
+        <RouterLink :to="link.parent.href">
+          <CategoryCard :categoryTitle="link.parent.name" :imageSource="categoryImages[index]" />
+        </RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '@/styles/constants.scss';
+@use '@styles/mixins.scss';
 
 .title {
   display: flex;
   flex-direction: column;
+
   font-family: constants.$font-heading;
-  color: constants.$color-primary;
   font-size: 4rem;
+  color: constants.$color-primary;
   text-transform: uppercase;
 
   &_upper-line {
@@ -99,8 +112,8 @@ const titleGreeting = ['Welcome', 'Sustainability and style', 'Discover organic 
   }
 }
 
-.carousel-wrapper {
-  padding: 1rem 0;
+.wrapper {
+  padding: 3rem 0;
 }
 
 .carousel {
@@ -118,28 +131,105 @@ const titleGreeting = ['Welcome', 'Sustainability and style', 'Discover organic 
 }
 
 .container {
-  padding: 0;
+  padding: 1rem;
   color: constants.$color-text-light;
   border-radius: 10px;
 }
 
-.wrapper {
+a {
+  font-size: 1.2rem;
+  transition: 0.3s;
+}
+
+a:hover {
+  color: constants.$color-secondary;
+  transition: 0.3s;
+}
+
+.carousel-item-wrapper {
   display: flex;
-  align-items: end;
-  width: 50rem;
+  flex-direction: column;
+  align-items: start;
+  justify-content: space-around;
+
+  width: 50%;
   height: 100%;
   padding: 5rem;
+
+  background-color: constants.$color-background-opacity;
 }
 
 .title-greeting {
+  font-family: constants.$font-heading;
   font-size: 4rem;
-  font-family: constants.$font-base;
   color: constants.$color-text-dark;
 }
 
 .carousel-text {
-  color: constants.$color-text-dark;
+  font-family: constants.$font-heading;
   font-size: 3rem;
-  font-family: constants.$font-base;
+  color: constants.$color-text-dark;
+}
+
+.advantages-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.9rem;
+  justify-content: space-between;
+}
+
+.categories {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.9rem;
+  justify-content: space-between;
+}
+
+@include mixins.media-tablet {
+  .categories {
+    align-items: center;
+    justify-content: center;
+  }
+
+  .advantages-wrapper {
+    justify-content: center;
+  }
+}
+
+@include mixins.media-middle {
+  .title {
+    font-size: 3rem;
+  }
+
+  .title-greeting,
+  .carousel-text {
+    font-size: 3rem;
+  }
+
+  .text {
+    font-size: 1rem;
+  }
+
+  .carousel-item-wrapper {
+    width: 50%;
+    padding: 4rem;
+  }
+}
+
+@include mixins.media-mobile {
+  .title-greeting,
+  .carousel-text {
+    font-size: 2rem;
+  }
+
+  .carousel-item-wrapper {
+    width: 100%;
+    padding: 2rem;
+  }
+}
+
+.title-category {
+  padding-bottom: 1rem;
+  color: constants.$color-primary;
 }
 </style>
---
+++ src/pages/MainPage/components/AdvantageCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string
+  Icon: object
+}>()
+</script>
+
+<template>
+  <div class="advantages-container">
+    <Icon />
+    <div>{{ props.text }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/styles/constants.scss';
+
+.advantages-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 15rem;
+  padding: 1.5rem;
+
+  border: 1px solid constants.$color-primary;
+  border-radius: 10px;
+}
+</style>
